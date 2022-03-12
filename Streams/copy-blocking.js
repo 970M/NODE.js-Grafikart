542021d593fd47dd77c89b50f4ef1d9f2c510b06
970M <970M@protonmail.com>
2022-03-11 20:51:39 -0400

    feat(Streams): copy-blocking.js copy-blocking.js completed

---
+++ Streams/copy-blocking.js
@@ -0,0 +1,15 @@
+let fs = require("fs");
+
+// Methode pas tres performante, data contient tout le fichier (processus bloquant: attendre la fin de la lecture pour lancer l'ecriture)
+fs.readFile("video.mp4", (err, data) => {
+    if (err) throw err;
+
+    fs.writeFile("video-copy.mp4", data, (err) => {
+        if (err) throw err;
+
+        console.log("Le fichier a bien été copié");
+    });
+});
+
+// https://devdocs.io/node/fs#fs.writeFile()
+// https://devdocs.io/node/fs#fs.createReadStream()
